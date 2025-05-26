10d7ae8094e39c9301b07dbf684df812f562958f
Park JongHun <thesn515@gmail.com>
2025-05-26 21:09:19 +0900

    initial commit

---
+++ main.rs
@@ -0,0 +1,7 @@
+// compile command is like `rustc foo.rs`
+// run command is like `./foo`
+fn main() {
+    // it's not function, but macro.
+    // so it must contains "!".
+    println!("Hello World!");
+}
---
+++ project_euler/problem_1.rs
@@ -0,0 +1,4 @@
+fn solve() {
+    let max: i8 = 1000;
+    let mut answer: i32;
+}
